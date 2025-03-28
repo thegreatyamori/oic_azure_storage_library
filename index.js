d1af8ff0b060e98b04066a95df752f9fdde01267
thegreatyamori <tod.jerson.morocho@gmail.com>
2025-03-28 18:33:13 -0500

    improve handler

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 const { getSharedKey } = require("./OIC/oic_hmac_authentication.js");
+require("dotenv").config();
 
-const accountKey = "ACCOUNT_KEY"
-const baseURL = "https://ACCOUNTNAME.file.core.windows.net";
+const accountKey = process.env.ACCOUNT_KEY;
+const accountName = process.env.ACCOUNT_NAME;
+const fileShare = process.env.BUCKET;
+console.log(fileShare);
+
+const baseURL = `https://${accountName}.file.core.windows.net`;
 const request = {
   list_all_files: {
-    account_name: "ACCOUNTNAME",
+    account_name: accountName,
     account_key: accountKey,
     origin_directory: "",
     destination_directory: "",
-    endpoint_url: `${baseURL}/BUCKET/Interface/Upload?comp=list&restype=directory`,
+    endpoint_url: `${baseURL}/${fileShare}/Interface/Upload?comp=list&restype=directory`,
     http_method: "GET",
   },
   read_single_file: {
-    account_name: "ACCOUNTNAME",
+    account_name: accountName,
     account_key: accountKey,
     origin_directory: "",
     destination_directory: "",
-    endpoint_url: `${baseURL}/BUCKET/Interface/Upload/Shipping-2025-03-27@02-00-06.shxupl`,
+    endpoint_url: `${baseURL}/${fileShare}/Interface/Upload/Shipping-2025-03-27@02-00-06.shxupl`,
     http_method: "GET",
   },
   copy_file: {
-    account_name: "ACCOUNTNAME",
+    account_name: accountName,
     account_key: accountKey,
-    origin_directory: "/BUCKET/Interface/Input/dummy_file.txt",
-    destination_directory: "/BUCKET/Interface/Upload/dummy_file.txt",
+    origin_directory: `/${fileShare}/Interface/Input/dummy_file.txt`,
+    destination_directory: `/${fileShare}/Interface/Upload/dummy_file.txt`,
     endpoint_url: baseURL,
     http_method: "PUT",
   },
   delete_file: {
-    account_name: "ACCOUNTNAME",
+    account_name: accountName,
     account_key: accountKey,
-    origin_directory: "/BUCKET/Interface/Input/dummy_file.txt",
+    origin_directory: `/${fileShare}/Interface/Input/dummy_file.txt`,
     destination_directory: "",
     endpoint_url: baseURL,
     http_method: "DELETE",
   },
 };
 
-const currentRequest = request.delete_file;
+const currentRequest = request.read_single_file;
 const rawSharedKey = getSharedKey(...Object.values(currentRequest));
 const [date, sharedKey] = rawSharedKey.split("||");
-let url = new URL(currentRequest.endpoint_url + currentRequest.destination_directory);
+let url = new URL(
+  currentRequest.endpoint_url + currentRequest.destination_directory
+);
 if (currentRequest.http_method === "DELETE") {
-    url = new URL(currentRequest.endpoint_url + currentRequest.origin_directory);
+  url = new URL(currentRequest.endpoint_url + currentRequest.origin_directory);
 }
 console.log(sharedKey);
 
@@ -50,10 +57,10 @@ const myHeaders = new Headers();
 myHeaders.append("x-ms-version", "2019-02-02");
 myHeaders.append("x-ms-date", date);
 if (currentRequest.http_method === "PUT") {
-    myHeaders.append(
-      "x-ms-copy-source",
-      currentRequest.endpoint_url + currentRequest.origin_directory
-    );
+  myHeaders.append(
+    "x-ms-copy-source",
+    currentRequest.endpoint_url + currentRequest.origin_directory
+  );
 }
 myHeaders.append("Authorization", sharedKey);
 myHeaders.append("Host", url.host);
@@ -66,6 +73,6 @@ const requestOptions = {
 };
 
 fetch(url, requestOptions)
-  .then((response) => console.log(response.text()))
+  .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
